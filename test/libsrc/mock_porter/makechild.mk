cfaf464201504b4053005dddece561677c8babd9
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-05 08:19:46 +0900

    pdb分散問題の対策

--- test/libsrc/mock_porter/makechild.mk
+++ test/libsrc/mock_porter/makechild.mk
@@ -1,2 +1,7 @@
 # サブフォルダはコンパイルのみ
 NO_LINK = 1
+
+# 静的ライブラリの場合に指定: サブフォルダのターゲットを明示的に指定
+# (Windows 環境で、pdb がサブフォルダ名単位で生成されないようにする)
+THIS_MAKEFILE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+TARGET := $(notdir $(patsubst %/,%,$(THIS_MAKEFILE_DIR)))
