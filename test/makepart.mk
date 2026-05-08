56a1a01b6f5d69a055af66fd197414e647c4a95d
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-05-09 06:28:00 +0900

    ビルドシステム改善

--- test/makepart.mk
+++ test/makepart.mk
@@ -1,15 +1,3 @@
-# インクルードの検索パス
-INCDIR += \
-	$(MYAPP_DIR)/../com_util/test/include \
-	$(MYAPP_DIR)/test/include \
-	$(TESTFW_DIR)/gtest/include \
-	$(TESTFW_DIR)/include
-
-# ライブラリの検索パス
-LIBSDIR += \
-	$(MYAPP_DIR)/test/lib \
-	$(MYAPP_DIR)/../com_util/test/lib
-
 ifdef PLATFORM_WINDOWS
     # 外部関数の static 定義
     CFLAGS   += /DPOTR_STATIC /DCOM_UTIL_STATIC
