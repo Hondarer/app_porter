3debe8ea32ed173d1aa4a30ec68d70fbc9ef3c9f
Tetsuo Honda <t-honda@hondarer-soft.com>
2026-08-09 15:03:24 +0900

    porter テストの動的ライブラリ探索パスを更新

--- test/makepart.mk
+++ test/makepart.mk
@@ -1,5 +1,6 @@
 ifdef PLATFORM_WINDOWS
-    # 外部関数の static 定義
+    # mock_porter と mock_com_util をテスト実行体へ直接定義する。
+    # 製品ライブラリのリンク方式は変更しない。
     CFLAGS   += /DPOTR_STATIC /DCOM_UTIL_STATIC
     CXXFLAGS += /DPOTR_STATIC /DCOM_UTIL_STATIC
 endif
